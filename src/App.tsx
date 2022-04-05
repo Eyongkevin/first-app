6b324b047d570c4bd0908fb48d84e30162b973f4
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2022-04-05 21:22:50 +0100

    add many configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import renderIf from 'render-if'
 import './App.css';
 
+import Customer from './components/Customer';
+
 function App() {
+  const [title, setTitle] = useState('Practical React Enterprise')
+
+  // #CSSinJS
+  const h1_style = {
+    color: 'blue',
+    marginBottom: '5rem'
+  }
+  const Render = true;
+
+  const showCustomerData = renderIf(Render)
+
+  const isPalindrome = (word: string) : Boolean=>  {
+    const smallCaseWord = word.toLowerCase()
+    const reversedWord = smallCaseWord.split('').reverse().join('')
+    return reversedWord === smallCaseWord
+
+  }
+
+  useEffect(() =>{
+    console.log("Welcome to the Practical React Enterprise!")
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1
+        onClick={() => setTitle("Become an Effective Developer from Day 1")}
+        style={h1_style}
         >
-          Learn React
-        </a>
-      </header>
+          {title}
+        </h1>
+        {
+          showCustomerData(
+            <Customer
+              firstName='Kevin'
+              lastName='Enow'
+              age={21} />
+          )
+        }
+      <p>Is 'Welcome' a palindrome?  </p>
+      { isPalindrome('Welcome')? <span>True</span>:<span>False</span>}
     </div>
   );
 }
---
+++ src/components/Customer.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+interface IPerson {
+  firstName: string
+  lastName: string
+  age: number
+  isLegal?: boolean
+}
+
+// type TPerson = {
+//     firstName: string;
+//     lastName: string;
+//     age: number;
+//     isLegal?: boolean;
+// };
+
+const Customer = (props: IPerson) => {
+  const { firstName, lastName, age } = props
+  const attrs = {
+    className: 'tem',
+    id: `tem-${1}` // template literals
+  }
+
+  return (
+    <div {...attrs}>
+      {' '}
+      {/* spread attribute operator */}
+      <h1> Hello React</h1>
+      <p>
+        My names are {firstName} {lastName} and I am {age} years old
+      </p>
+    </div>
+  )
+}
+
+export default Customer
